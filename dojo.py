898fb67d6d9f3e5ceebe8430002a9be6cfd7cd6a
KevinPham <kevinpham15@gmail.com>
2017-10-08 23:19:57 -0700

    completed

---
+++ dojo.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    print "Sumbitted Infomation"
+    name = request.form ['name']
+    location = request.form ['location']
+    language = request.form ['language']
+    comment = request.form ['comment']
+    return render_template('results.html', name = name, location = location, language = language, comment = comment)
+app.run(debug=True)
